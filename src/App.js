f19aed8b0ed760f9267478d522afd0541397c478
Roman Usherenko <roman.usherenko@gmail.com>
2017-07-25 18:06:05 +0300

    do not vendor bootstrap css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Navbar, Panel, Button } from 'react-bootstrap';
 
+import 'bootstrap-css-only';
+
 class App extends Component {
   render() {
     return (
